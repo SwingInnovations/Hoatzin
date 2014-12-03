15fea2ad9f6101f0d7052d95a09bfac42c090659
DanqueDynasty <natecgxpstudio@hotmail.com>
2014-12-02 17:44:06 -0800

    0.0.46.6
    Expanded the Light implementation.

--- Utility/SWLight.h
+++ Utility/SWLight.h
@@ -6,6 +6,33 @@
 
 class SWLight : public SWComponent{
 public:
+	void addDirectionalLight(Vector3f position, Vector3f intensity, float amb){
+		light.position = Vector4f(position.getX(), position.getY(), position.getZ(), 0.0f);
+		light.intensity = intensity;
+		light.ambientCoefficient = amb;
+		addLightUniforms(0, "position", SWShader::VEC4, SWShader::toString(light.position));
+		addLightUniforms(0, "intensity", SWShader::VEC3, SWShader::toString(light.intensity));
+		addLightUniforms(0, "ambientCoefficient", SWShader::FLOAT, SWShader::toString(light.ambientCoefficient));
+
+	}
+
+	void addSpotLight(Vector3f position, Vector3f intensity, float at, float amb, float coneAngle){
+		light.position = Vector4f(position.getX(), position.getY(), position.getZ(), 1.0);
+		light.intensity = intensity;
+		light.attenuation = at;
+		light.ambientCoefficient = amb;
+		light.coneAngle = coneAngle;
+		addLightUniforms(0, "position", SWShader::VEC4, SWShader::toString(light.position));
+		addLightUniforms(0, "intensity", SWShader::VEC3, SWShader::toString(light.intensity));
+		addLightUniforms(0, "ambientCoefficient", SWShader::FLOAT, SWShader::toString(light.ambientCoefficient));
+	}
+
+	void addLightUniforms(int num, const std::string& propertyName, int type, const std::string& propertyVal){
+		std::ostringstream output;
+		output << "SWLight[" << num << "]." << propertyName.c_str();
+		std::string info = output.str();
+		uniforms.push_back(SWShader::ShaderInfo(info, type, propertyVal));
+	}
 
 	Light light;
 };
