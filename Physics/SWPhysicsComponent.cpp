c2c96708bc67e5cba8da89a185b65b55e0de03fe
DanqueDynasty <natecgxpstudio@hotmail.com>
2014-12-16 19:24:24 -0800

    0.0.47.5
    Started implementing physics

---
+++ Physics/SWPhysicsComponent.cpp
@@ -0,0 +1,18 @@
+/*
+ * SWPhysicsComponent.cpp
+ *
+ *  Created on: Dec 16, 2014
+ *      Author: Nathan_z400
+ */
+
+#include "SWPhysicsComponent.h"
+
+SWPhysicsComponent::SWPhysicsComponent() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SWPhysicsComponent::~SWPhysicsComponent() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Physics/SWPhysicsComponent.h
@@ -0,0 +1,11 @@
+
+#ifndef SWPHYSICSCOMPONENT_H_
+#define SWPHYSICSCOMPONENT_H_
+
+class SWPhysicsComponent {
+public:
+	SWPhysicsComponent();
+	virtual ~SWPhysicsComponent();
+};
+
+#endif /* SWPHYSICSCOMPONENT_H_ */
