26beb5c5283ce4be6e21762a3afde24d6af944ad
DanqueDynasty <natecgxpstudio@hotmail.com>
2015-03-04 20:07:13 -0800

    0.0.52.5
    Contining to debug frameBuffers

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -3,24 +3,25 @@
 SWRenderPass::SWRenderPass(){
 	sWidth = 0;
 	sHeight = 0;
-	glGenFramebuffers(1, &frameBuffer);
-	glGenRenderbuffers(1, &renderBuff);
-	glGenTextures(1, &texBuffer);
 	m = new Mesh(new Plane(0, 0, 1, 1));
 	s = new Shader("Shader/screen", "Shader/default");
 
+	glGenFramebuffers(1, &frameBuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
 
+	glGenTextures(1, &texBuffer);
 	glBindTexture(GL_TEXTURE_2D, texBuffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, sWidth, sHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_BGR, sWidth, sHeight, 0, GL_BGR, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texBuffer, 0);
 
+	glGenRenderbuffers(1, &renderBuff);
 	glBindRenderbuffer(GL_RENDERBUFFER, renderBuff);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, sWidth, sHeight);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
-	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texBuffer, 0);
 	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBuff);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -39,17 +40,20 @@ SWRenderPass::SWRenderPass(int x, int y){
 	glGenTextures(1, &texBuffer);
 	glBindTexture(GL_TEXTURE_2D, texBuffer);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, sWidth, sHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texBuffer, 0);
 
 	glGenRenderbuffers(1, &renderBuff);
 	glBindRenderbuffer(GL_RENDERBUFFER, renderBuff);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, sWidth, sHeight);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBuff);
 	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texBuffer, 0);
-	glDrawBuffer(GL_COLOR_ATTACHMENT0);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBuff);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
@@ -64,15 +68,12 @@ SWRenderPass::~SWRenderPass(){
 void SWRenderPass::draw(Camera* cam){
 	std::cout << "Drawing in Progress" << std::endl;
 	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-	glEnable(GL_DEPTH_TEST);
 
 	for(int i = 0; i < (int)objects.size(); i++){
 		objects[i]->draw(cam);
 	}
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glDisable(GL_DEPTH_TEST);
 	s->bind();
 	glEnable(GL_TEXTURE_2D);
 	glActiveTexture(GL_TEXTURE0);
